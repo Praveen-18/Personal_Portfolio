96ad81e621e9b0f218e5d1b79005104cbcbccf33
praveen <prav20ad036@rmkcet.ac.in>
2023-03-12 23:48:17 +0530

    Contact form added :octocat:

--- spike/views.py
+++ spike/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from .models import contact
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        comments = request.POST.get('comments')
+        print(name , email , comments)
+        current_contact = contact(name=name, email=email, comments=comments)
+        current_contact.save()
+        return render(request, 'spike/index.html')
     return render(request, 'spike/index.html')
+
+
+
